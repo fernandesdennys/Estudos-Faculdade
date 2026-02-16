d84c65f9f6350a416a316b142fdbd2e223c137e4
Dennys Fernandes <dennys.projetos@outlook.com>
2026-02-16 14:49:00 -0300

    Aula de Java - declaração e uso de variáveis

---
+++ java/estruturas-basicas-java/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args)  {
+        /*
+        * sintaxe de declaração das variáveis no Java
+        * <tipo> iddentificador;
+        * <tipo> iddentificador = > valor;
+        * 
+        * Variáveis tem escopos: blocos de comandos no qual a variável foi declarada
+        *  
+        */
+       String saudacao = "Olá, Dennys";
+       System.out.print(saudacao);
+    }
+}
