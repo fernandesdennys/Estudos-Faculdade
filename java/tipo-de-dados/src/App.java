0bdfd3c772e971c6ff79f4cd3d3880480b1cae71
Dennys Fernandes <dennys.projetos@outlook.com>
2026-02-16 16:56:21 -0300

    Conheca os tipos primitivos no JAVA

---
+++ java/tipo-de-dados/src/App.java
@@ -0,0 +1,56 @@
+public class App {
+    public static void main(String[] args) {
+        /*
+        Tipos numéricos inteiros
+        byte num1 = 1;
+        short num2 = 10000;
+        int num3 = 1000000000;
+        long num4 = 100_000_000_000_00L;
+
+        int numero1 = 10
+        int numero2 = 20
+        int soma = numero1 + numero2;
+        */
+
+       //Tipos decimais - float, double
+       float numero1 = 1.5f;
+       float numero2 = 1.5f;
+       float soma = numero1 + numero2;
+       System.out.println(soma);
+
+       double numero3 = 2.5;
+       double numero4 = 2.5;
+       
+       double soma2 = numero3 + numero4;
+       System.out.println(soma2);
+
+       double mult = numero3 * numero4;
+       System.out.println(mult);
+
+       double div = numero3 / numero4;
+       System.out.println(div);
+
+       double sub = numero3 - numero4;
+       System.out.println(sub);
+
+       
+       // Char e boolean
+       char operador = '+';
+
+       double numero5 = 3.5;
+       double numero6 = 6.5;
+
+       double resultado = soma(4.6, 5.4 );
+       System.out.println(operador + " : " + resultado);
+
+       boolean ehIgual = numero5 == numero6;
+       boolean ehMaior = numero5 < numero6;
+       
+       System.out.println(ehIgual);
+       System.out.println(ehMaior);
+    }
+
+    static double soma (double numero1, double numero2) {
+        return numero1 + numero2;
+    }
+}
