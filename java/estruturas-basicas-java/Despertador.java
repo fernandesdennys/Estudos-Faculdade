cf735cb8dbaa1153f3aa150ce32b317d6416cfc4
Dennys Fernandes <dennys.projetos@outlook.com>
2026-02-16 15:49:35 -0300

    Aprendendo a usar classe dentro de JAVA

---
+++ java/estruturas-basicas-java/Despertador.java
@@ -0,0 +1,15 @@
+public class Despertador {
+  int horas;
+  int minutos;
+  int soneca;
+
+  void  ativarSoneca() {
+    // Só mias .... minutos
+    System.out.println("Só mais" + soneca + "minutos");
+  }
+
+  String agendarAlarme() {
+    // Seu  alarme foi configurado  às 12:30
+    return "Seu  alarme foi configurado  às " + hora + ":" + minutos;
+}
+}
