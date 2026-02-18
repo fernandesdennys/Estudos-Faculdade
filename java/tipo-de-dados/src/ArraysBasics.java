e5472e1f33693658a616d45a8ed4234d99201a61
Dennys Fernandes <dennys.projetos@outlook.com>
2026-02-18 15:51:21 -0300

    Aprendendo a usar ARRAYS no JAVA

---
+++ java/tipo-de-dados/src/ArraysBasics.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArraysBasics {
+  public static void main(String[] args) {
+    // Sintaxe ==> <tipo> [] identificador = {valores}
+    /* String [] tarefas = {"Acordar", "Tomar café", "Escovar os dentes", "Lêr um livro", "Ir a academia"}; */
+
+/*     String [] tarefas = new String[5];
+    tarefas[0] = "Acordar";
+    tarefas[1] = "Tomar café";
+    tarefas[2] = "Escovar os dentes";
+    tarefas[3] = "Lêr um livro";
+    tarefas[4] = "Ir a academia";
+
+    System.out.println(tarefas[0]);
+    System.out.println(tarefas[1]);
+    System.out.println(tarefas[2]);
+    System.out.println(tarefas[3]);
+    System.out.println(tarefas[4]);
+    System.out.println(Arrays.toString(tarefas));
+    System.out.println(tarefas.length);
+ */
+
+    ArrayList<String> tarefas = new ArrayList<>();
+    tarefas.add("Acordar");
+    tarefas.add("Tomar café");
+    tarefas.add("Escovar os dentes");
+    tarefas.add("Lêr um livro");
+    tarefas.add("Ir a academia");
+
+    System.out.println(tarefas);
+    System.out.println(tarefas.size());
+    System.out.println(tarefas.get(0));
+  }
+}
--- java/tipo-de-dados/src/Teste.java
+++ java/tipo-de-dados/src/Teste.java
@@ -2,7 +2,7 @@ import br.com.dennysfernandes.lugares.Biblioteca;
 
 public class Teste {
   public static void main(String[] args) {
-    Piscina piscina;
+    /* Piscina piscina; */
     Biblioteca biblioteca = new Biblioteca();
     System.out.println(biblioteca.baneheiro);
   }
