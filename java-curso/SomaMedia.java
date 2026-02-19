02a9b45768d1a72b85d7a2eedf8a3f364fa0547f
Dennys Fernandes <dennys.projetos@outlook.com>
2026-02-19 15:03:51 -0300

    Aula 1 - primeiros ex. com Java

---
+++ java-curso/SomaMedia.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SomaMedia {
+  public static void main (String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    
+    System.out.println("Digite o primeiro numero");
+    double a = entrada.nextDouble();
+    
+    System.out.println("Digite o segundo numero");
+    double b = entrada.nextDouble();
+    
+    double media = (a + b) / 2;
+    System.out.println("Voce digitou " + a + " e " + b + ", a média é: " + media);
+    
+    entrada.close(); 
+    
+  }
+}
+
